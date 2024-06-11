d0bd57489835453139e13811cb1524210459279b
Jessi <jessicarosa.cedesp@gmail.com>
2024-06-10 21:28:49 -0300

    Add files via upload

---
+++ assests/js/script2.js
@@ -0,0 +1,52 @@
+// Chave da api
+const key = 'df76991302551e2743f23d05555bf312';
+
+const button = document.querySelector('.botao_buscar');
+const nomeCidade =  document.querySelector('.cidade');
+const temperatura = document.querySelector('.tempo'); 
+const previsao =  document.querySelector('.descricao'); 
+const umidade =  document.querySelector('.umidade'); 
+const imagem =  document.querySelector('.icone'); 
+
+// URL DA API OPEN WEATHER
+// `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`
+
+
+ function colocaDadosNaTela(dados){
+
+    nomeCidade.innerHTML = dados.name
+    temperatura.innerHTML = Math.floor(dados.main.temp) + "°C"
+    previsao.innerHTML = dados.weather[0].description;
+    umidade.innerHTML = "umidade: " + dados.main.humidity + "%";
+    imagem.scr = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`;
+
+    console.log(dados);
+};
+
+// BUSCAR CIDADE
+async function buscarCidade(cidade) {
+
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json());
+
+
+    colocaDadosNaTela(dados);
+}
+
+// FUNÇÂO procurar
+function cliqueBotao() {
+    const cidade = document.querySelector('.input_cidade').value;
+    buscarCidade(cidade);
+}
+
+
+button.addEventListener('click', cliqueBotao );
+
+document.addEventListener("keypress", (event) =>{
+    
+    if (event.key === "Enter"){
+
+        const cidade = document.querySelector('.input_cidade').value;
+        buscarCidade(cidade);
+    }
+
+});
